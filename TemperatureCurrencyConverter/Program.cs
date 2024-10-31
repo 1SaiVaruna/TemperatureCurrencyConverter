2ebfb540a6c6f767c8bd88b3383daea7a0d671c5
1SaiVaruna <sai030804@gmail.com>
2024-10-31 21:25:53 +0000

    Added main menu options for temperature/currency conversions and exit.

--- TemperatureCurrencyConverter/Program.cs
+++ TemperatureCurrencyConverter/Program.cs
@@ -4,7 +4,22 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.Clear();
+			Console.Title = "TEMPERATURE & CURRENCY CONVERTER";
+			Console.WriteLine("TEMPERATURE / CURRENCY CONVERTER");
+			Console.WriteLine("---------------------------------\n");
+			Console.WriteLine("- [T] TEMPERATURE CONVERTER\n");
+			Console.WriteLine("- [C] CURRENCY CONVERTER\n");
+			Console.WriteLine("- [X] EXIT\n");
+			Console.Write("ENTER OPTION: ");
+			string answer = Console.ReadLine().ToUpper();
+
+			if (answer == "X")
+			{
+				break;
+			}
 		}
+
 	}
 }
+
